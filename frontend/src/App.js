4b49229aed49f5075cda82697d34f3e8bea44409
Jonathan Hu <hujonathan89@gmail.com>
2018-04-02 01:07:38 -0700

    Add no match routes, WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Route, withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { Route, withRouter, Switch } from "react-router-dom";
 
 // components
-import TopNav from './components/TopNav';
-import ListView from './components/ListView';
+import TopNav from "./components/TopNav";
+import ListView from "./components/ListView";
 // actions
-import { fetchCategories } from './actions';
+import { fetchCategories } from "./actions";
+
 
 class App extends Component {
   componentWillMount() {
-    this.props.loadCategories()
+    this.props.loadCategories();
   }
 
   render() {
-    const { categories } = this.props
+    const { categories } = this.props;
+
     return (
       <MuiThemeProvider>
         <div>
           <TopNav categories={categories} />
-          <Route exact path='/' component={() => (
-            <ListView />
-          )} />
-          <Route exact path='/:category' component={(match) => (
-            <ListView match={match} />
-          )} />
-          <Route exact path='/:category/:postId' component={(match) => (
-            <ListView match={match} />
-          )} />
+          <Switch>
+            <Route exact path="/" component={() => <ListView />} />
+            <Route
+              exact
+              path="/:category"
+              component={match => <ListView match={match} />}
+            />
+            <Route
+              exact
+              path="/:category/:postId"
+              component={match => <ListView match={match} />}
+            />
+          </Switch>
         </div>
       </MuiThemeProvider>
     );
@@ -39,13 +45,15 @@ class App extends Component {
 function matchStateToProps({ categories }) {
   return {
     categories
-  }
+  };
 }
 
 function matchDispatchToProps(dispatch) {
   return {
     loadCategories: () => dispatch(fetchCategories())
-  }
+  };
 }
 
-export default withRouter(connect(matchStateToProps, matchDispatchToProps)(App));
+export default withRouter(
+  connect(matchStateToProps, matchDispatchToProps)(App)
+);
---
+++ frontend/src/components/NoMatch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoMatch(props) {
+  return (
+    <div>
+      <h1>Post Not Found</h1>
+    </div>
+  );
+}
+
+export default NoMatch;
